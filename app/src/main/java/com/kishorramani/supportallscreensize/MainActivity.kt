785b4872d55de5b4000b9c2bd11a7fcd916c5f13
kishor ramani <kishorkeepsmiling@gmail.com>
2021-01-19 05:09:29 +0530

    Initial and Final commit

---
+++ app/src/main/java/com/kishorramani/supportallscreensize/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kishorramani.supportallscreensize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
